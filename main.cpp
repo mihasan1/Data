6b3b64aa869935f2544580f2527913528a2608c4
Mihasan <mihasan1@outlook.com>
2020-03-14 18:48:41 +0200

    Threed commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
+
+int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 
 class Date
 {
@@ -14,14 +17,11 @@ public:
 		month = 0;
 		year = 0;
 	};
-	Date(int d, int m, int y)
+	Date(char(&data)[11])
 	{
-		if (TrueDate(d, m, y))
-		{
-			day = d;
-			month = m;
-			year = y;
-		}
+		SeperateSTR(data);
+		if (!TrueDate())
+			cout << "Дата введена неправильно!";
 	}
 
 	~Date()
@@ -29,14 +29,47 @@ public:
 
 	}
 
+	void SeperateSTR(char(&data)[11])
+	{
+		string tmp = "";
+		int k = 0;
+		for (int i = 0; i < 11; i++)
+		{
+			if (i == 10)
+			{
+				year = (atoi(tmp.c_str()));
+			}
+			if (data[i] != '.')
+			{
+				tmp += data[i];
+			}
+			else
+			{
+				switch (k)
+				{
+				case 0:
+					day = (atoi(tmp.c_str()));
+					break;
+				case 1:
+					month = (atoi(tmp.c_str()));
+					break;
+				default:
+					break;
+				}
+				k++;
+				tmp = "";
+			}
+		}
+	}
+
 	void print() const
 	{
 		cout << day << '.' << month << '.' << year << std::endl;
 	}
 
-	bool TrueDate(int d, int m, int y)
+	bool TrueDate()
 	{
-		if ((d < 1 || d>dayInMonth(m)) || (m < 1 || m>12))
+		if ((day < 1 || day>dayInMonth(month)) || (month < 1 || month>12))
 		{
 			cout << "Дата введена неправильно" << endl;
 			return false;
@@ -46,7 +79,7 @@ public:
 
 	bool isLeapYear()
 	{
-		if (year % 4 == 0)
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 			return true;
 		else
 			return false;
@@ -54,14 +87,12 @@ public:
 
 	int dayInMonth(int month)
 	{
-		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (isLeapYear())days[1] = 29;
 		return days[month - 1];
 	}
 
 	void AddDays(int g)
 	{
-		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (isLeapYear())days[1] = 29;
 		day += g;
 		while (day > days[month - 1])
@@ -75,9 +106,8 @@ public:
 			}
 		}
 	}
-	void DecreaseDays(int g)
+	void SubstractDays(int g)
 	{
-		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (isLeapYear()) days[1] = 29;
 		while (g > 0)
 		{
@@ -92,7 +122,7 @@ public:
 				}
 				day = days[month - 1];
 			}
-			else if(day==g)
+			else if (day == g)
 			{
 				day -= g;
 				g = 0;
@@ -113,23 +143,22 @@ public:
 	}
 	void AddMonth(int g)
 	{
-		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (isLeapYear()) days[1] = 29;
-		/*month += g;
+		month += g;
 		while (month > 12)
 		{
 			++year;
-			month -=12;
+			month -= 12;
 		}
-		if (day > days[month-1])
+		if (day > days[month - 1])
 		{
-			AddDays(days[month-1] - day);
-		}*/
-		int days_to_add = 0;
+			AddDays(days[month - 1] - day);
+		}
+		/*int days_to_add = 0;
 		int i = 0;
 		while (g > 0)
 		{
-			for (i = month - 1; i < 12; i++)
+			for (i = month; i <12; i++)
 				days_to_add += days[i];
 			g--;
 		}
@@ -139,65 +168,162 @@ public:
 				days_to_add += days[i];
 			g--;
 		}
-		AddDays(days_to_add);
+		AddDays(days_to_add);*/
+	}
+	void SubstractMonths(int g)
+	{
+		if (isLeapYear()) days[1] = 29;
+		while (g > 0)
+		{
+			g--;
+			month--;
+			if (month < 1)
+			{
+				year--;
+				month = 12;
+			}
+			if (day > days[month - 1])
+			{
+				AddDays(days[month - 1] - day);
+			}
+		}
+	}
+	void AddYears(int g)
+	{
+		year += g;
+	}
+	void SubstractYears(int g)
+	{
+		year -= g;
+	}
+	bool CompareDates(Date cmp)
+	{
+		if (day < cmp.day || month < cmp.month || year < cmp.year)
+			return false;
+		else
+			return true;
+	}
+	int countDays(Date d2)
+	{
+		int count = 0;
+		while (!(day == d2.day && month == d2.month && year == d2.year))
+		{
+			if (CompareDates(d2))
+				SubstractDays(1);
+			else if (!CompareDates(d2))
+				AddDays(1);
+			count++;
+		}
+		return count;
+	}
+
+	float countOfMonths(Date d2)
+	{
+		return (countDays(d2)/30);
 	}
 };
+
 
 int main()
 {
 	system("chcp 1251");
-	char data[11];
+	char data[11], data2[11];
 	cout << "Введите дату в формате 'ДД.ММ.ГГГГ':\n";
 	cin.getline(data, 11);
-	int d = 0, m = 0, y = 0;
-	string tmp = "";
-	int k = 0;
 	int g;
-	for (int i = 0; i < 11; i++)
+	Date d1(data);
+	d1.print();
+	cout << "1 - Добавить к дате     2 - Отнять от даты (ДНИ)\n";
+	cout << "3 - Добавить к дате     4 - Отнять от даты (МЕСЯЦЫ)\n";
+	cout << "5 - Добавить к дате     6 - Отнять от даты (ГОДЫ)\n";
+	cout << "7 - Сравнить две даты   8 - Кол-во дней между датами\n";
+	cout << "9 - Кол-во месяцев между датами  0 - Выход\n";
+	while (true)
 	{
-		if (i == 10)
+		int choice;
+		cout << "Выберите действие: ";
+		cin >> choice;
+		switch (choice)
 		{
-			y = (atoi(tmp.c_str()));
+		case 1:
+			cout << "Введите кол-во дней для добавления: ";
+			cin >> g;
+			d1.AddDays(g);
+			d1.print();
+			break;
+		case 2:
+			cout << "Введите кол-во дней для вычитания: ";
+			cin >> g;
+			d1.SubstractDays(g);
+			d1.print();
+			break;
+		case 3:
+			cout << "Введите кол-во месяцев для добавления: ";
+			cin >> g;
+			d1.AddMonth(g);
+			d1.print();
+			break;
+		case 4:
+			cout << "Введите кол-во месяцев для вычитания: ";
+			cin >> g;
+			d1.SubstractMonths(g);
+			d1.print();
+			break;
+		case 5:
+			cout << "Введите кол-во лет для добавления: ";
+			cin >> g;
+			d1.AddYears(g);
+			d1.print();
+			break;
+		case 6:
+			cout << "Введите кол-во лет для вычитания: ";
+			cin >> g;
+			d1.SubstractYears(g);
+			d1.print();
+			break;
+		case 7:
+		{
+			cout << "Введите дату в формате 'ДД.ММ.ГГГГ':\n";
+			cin.ignore();
+			cin.getline(data2, 11);
+			Date d2(data2);
+			d2.print();
+			d1.CompareDates(d2);
+			if (d1.CompareDates(d2))
+				cout << data << " > " << data2 << endl;
+			else
+				cout << data << " < " << data2 << endl;
+
+			d2.~Date();
 		}
-		if (data[i] != '.')
+		break;
+		case 8:
 		{
-			tmp += data[i];
+			cout << "Введите дату в формате 'ДД.ММ.ГГГГ':\n";
+			cin.ignore();
+			cin.getline(data2, 11);
+			Date d2(data2);
+			cout << "Между " << data << " и " << data2 << " " << abs(d1.countDays(d2)) << " дней\n";
+			d2.~Date();
 		}
-		else
+		break;
+		case 9:
 		{
-			switch (k)
-			{
-			case 0:
-				d = (atoi(tmp.c_str()));
-				break;
-			case 1:
-				m = (atoi(tmp.c_str()));
-				break;
-			default:
-				break;
-			}
-			k++;
-			tmp = "";
+			cout << "Введите дату в формате 'ДД.ММ.ГГГГ':\n";
+			cin.ignore();
+			cin.getline(data2, 11);
+			Date d2(data2);
+			cout << "Между " << data << " и " << data2 << " " << abs(d1.countOfMonths(d2)) << " месяца(-ев)\n";
+			d2.~Date();
+		}
+		break;
+		case 0:
+			return 0;
+		default:
+			cout << "Неправильно!";
+			break;
 		}
 	}
-	Date d1(d, m, y);
-	if (d1.TrueDate(d, m, y))
-		d1.print();
-	else
-		return 0;
-
-	cout << "Введите кол-во дней для добавления: ";
-	cin >> g;
-	d1.AddDays(g);
-	d1.print();
-	cout << "Введите кол-во дней для вычитания: ";
-	cin >> g;
-	d1.DecreaseDays(g);
-	d1.print();
-	cout << "Введите кол-во месяцев для добавления: ";
-	cin >> g;
-	d1.AddMonth(g);
-	d1.print();
 
 	return 0;
 }
